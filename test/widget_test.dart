8d4976c92d71b609609b77c58649d487f1b086f7
Finn-Ole Kahl <finn-ole.kahl@gmx.de>
2024-05-03 13:44:43 +0200

    fix smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,32 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pulse_pro/bloc/app_state_bloc.dart';
 import 'package:pulse_pro/main.dart';
+import 'package:pulse_pro/repositories/authencitation_repository.dart';
+import 'package:pulse_pro/repositories/user_repository.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const PulseProApp());
+    await tester.pumpWidget(MultiRepositoryProvider(
+    providers: [
+      RepositoryProvider(
+        create: (context) => AuthenticationRepository(),
+      ),
+      RepositoryProvider(
+        create: (context) => UserRepository(),
+      ),
+    ],
+    child: BlocProvider(
+      lazy: false,
+      create: (context) => AppStateBloc(
+        userRepository: context.read<UserRepository>()),
+      child: const PulseProApp(),
+    ),
+  ));
 
     // Verify that our counter starts at 0.
     expect(find.text('Pulse Pro'), findsOneWidget);
