8d3e0d207527e6ce5f55a508eabffba1b84def25
Chris Grabener <chris17.grabener@gmail.com>
2024-05-03 13:30:01 +0200

    Home Page Top + Bottom App Bar

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -5,7 +5,7 @@ import 'package:pulse_pro/features/home/cubit/home_cubit.dart';
 import 'package:pulse_pro/repositories/authencitation_repository.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +14,18 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Fitness App'),
+          backgroundColor: Color.fromARGB(255, 150, 150, 150).withOpacity(0.01), // Durchsichtiger Hintergrund
         ),
-        body: const HomePageContent(),
+        body: const Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: HomePageContent(),
+              ),
+            ),
+            BottomBar(), // Bottom Bar außerhalb des SingleChildScrollView platzieren
+          ],
+        ),
         backgroundColor: Colors.grey[200], // Hintergrundfarbe ändern
       ),
     );
@@ -29,98 +39,131 @@ class HomePageContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    context.read<HomeCubit>().signOut();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red, // Ändere die Hintergrundfarbe des Buttons
-                    textStyle: const TextStyle(fontSize: 18), // Ändere die Schriftgröße des Textes
-                  ),
-                  child: const Text('Logout'),
-                ),
-                const SizedBox(height: 20), // Abstand zwischen den Buttons
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const TutorialPage()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue, // Ändere die Hintergrundfarbe des Buttons
-                    textStyle: const TextStyle(fontSize: 18), // Ändere die Schriftgröße des Textes
-                  ),
-                  child: const Text('Tutorial'),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.grey[300], // Hintergrundfarbe der Leiste
-          height: 100, // Vergrößere die Höhe der Leiste
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Aktion für Tagebuch ausführen
-                    },
-                    icon: const Icon(Icons.book),
-                  ),
-                  const SizedBox(height: 4), // Abstand unter dem Text
-                  const Text('Tagebuch'),
-                ],
+              ElevatedButton(
+                onPressed: () {
+                  context.read<HomeCubit>().signOut();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red, // Ändere die Hintergrundfarbe des Buttons
+                  textStyle: const TextStyle(fontSize: 18), // Ändere die Schriftgröße des Textes
+                ),
+                child: const Text('Logout'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Aktion für Trainingspläne ausführen
-                    },
-                    icon: const Icon(Icons.fitness_center),
-                  ),
-                  const SizedBox(height: 4), // Abstand unter dem Text
-                  const Text('Trainingspläne'),
-                ],
+              const SizedBox(height: 20), // Abstand zwischen den Buttons
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const TutorialPage()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // Ändere die Hintergrundfarbe des Buttons
+                  textStyle: const TextStyle(fontSize: 18), // Ändere die Schriftgröße des Textes
+                ),
+                child: const Text('Tutorial'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Aktion für Fortschritte ausführen
-                    },
-                    icon: const Icon(Icons.trending_up),
+              const Text(
+                'Willkommen zur Fitness App!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 0, 0, 0), // Textfarbe ändern
                   ),
-                  const SizedBox(height: 4), // Abstand unter dem Text
-                  const Text('Fortschritte'),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Aktion für Profil ausführen
-                    },
-                    icon: const Icon(Icons.person),
+                ),
+                const SizedBox(height: 20), // Abstand zum nächsten Text
+                const Text(
+                  'Entdecke eine neue Dimension des Trainings und der Gesundheit. olor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 0, 0, 0), // Textfarbe ändern
                   ),
-                  const SizedBox(height: 4), // Abstand unter dem Text
-                  const Text('Profil'),
-                ],
-              ),
+                ),
             ],
           ),
         ),
       ],
     );
   }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned( // Bottom Bar am unteren Rand positionieren
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: Container(
+        height: 100, // Höhe der Leiste
+        color: Colors.grey[300], // Hintergrundfarbe der Leiste
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // Aktion für Tagebuch ausführen
+                  },
+                  icon: const Icon(Icons.book),
+                ),
+                const SizedBox(height: 4), // Abstand unter dem Text
+                const Text('Tagebuch'),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // Aktion für Trainingspläne ausführen
+                  },
+                  icon: const Icon(Icons.fitness_center),
+                ),
+                const SizedBox(height: 4), // Abstand unter dem Text
+                const Text('Trainingspläne'),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // Aktion für Fortschritte ausführen
+                  },
+                  icon: const Icon(Icons.trending_up),
+                ),
+                const SizedBox(height: 4), // Abstand unter dem Text
+                const Text('Fortschritte'),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // Aktion für Profil ausführen
+                  },
+                  icon: const Icon(Icons.person),
+                ),
+                const SizedBox(height: 4), // Abstand unter dem Text
+                const Text('Profil'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
 }
