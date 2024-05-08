d5b1f5f6786814a783c5e745a95e66165b6baa3a
Malik Süzen <maliksuzen00@gmail.com>
2024-05-08 12:22:19 +0200

    oboarding

--- test/features/onboarding_view_test.dart
+++ test/features/onboarding_view_test.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+
+import '../test_helpers.dart';
+
+@GenerateNiceMocks([MockSpec<OnboardingCubit>()])
+
 
 void main(){
 
   group('OnboardingView', () { 
-    
-  })
+    testPulseProWidget('test onboarding view header', (tester) {
+      tester.pumpWidget(MaterialApp(
+        home: const OnboardingView(),
+      ));
+    });
+  });
 }
