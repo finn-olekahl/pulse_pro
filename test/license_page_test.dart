93e5ab19781f122fb37abfe1dd450be2fd1002c1
Malik Süzen <maliksuzen00@gmail.com>
2024-05-29 16:55:06 +0200

    license test

---
+++ test/license_page_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pulse_pro/features/licenses/license_page.dart';
+import 'package:pulse_pro/features/licenses/view/license_view.dart';
+
+
+void main() {
+  testWidgets('LicensesPage renders LicensesView', (WidgetTester tester) async {
+    // Pump the widget into the widget tree
+    await tester.pumpWidget(const MaterialApp(
+      home: LicensesPage(),
+    ));
+
+    // Verify that LicensesView is present in the widget tree
+    expect(find.byType(LicensesView), findsOneWidget);
+
+    // Print a message indicating the test has passed
+    print('LicensesView is present in the LicensesPage.');
+  });
+}
